8fb92e1755dce40927c2cec4c65d413578c1f872
yuyashiratori <91607152+yuyashiratori@users.noreply.github.com>
2021-10-02 21:06:46 +0900

    First Commit

---
+++ kadai11-2/src/FirePokemonCharacter.java
@@ -0,0 +1,8 @@
+
+public interface FirePokemonCharacter {
+
+	void attack(int hp1,int hp2);
+	void defense();
+	void FireAttack(int hp1,int hp2);
+	
+}
---
+++ kadai11-2/src/ThunderPokemonCharacter.java
@@ -0,0 +1,8 @@
+
+public interface ThunderPokemonCharacter {
+	
+	void attack(int hp1,int hp2);
+	void defense();
+	void ThunderAttack(int hp1,int hp2);
+
+}
